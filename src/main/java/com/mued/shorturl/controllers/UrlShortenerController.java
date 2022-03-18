604356832180570193f36a8c52a14a88e8b3c9e9
edmerdash01@gmail.com <Muhammed100M>
2022-03-18 10:48:03 +0200

    fix warnings

--- src/main/java/com/mued/shorturl/controllers/UrlShortenerController.java
+++ src/main/java/com/mued/shorturl/controllers/UrlShortenerController.java
@@ -10,9 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
-// we can include spring-data-rest instead of using controllers and comply to naming convension
+// we can include spring-data-rest instead of using controllers and comply to naming convension (Reading)
 
 @RestController
 @RequestMapping(value = "/api/v1")
@@ -22,13 +23,11 @@ public class UrlShortenerController {
     private UrlMappingService urlMappingService;
 
     /**
-     *
-     * @param shortUrl
+     * @param shortUrl :String
      * @return Returns the original URL.
      */
     @GetMapping(path = "/original/{shortUrl}")
-    @ResponseBody
-    public ResponseEntity getOriginalUrl(@PathVariable String shortUrl) {
+    public ResponseEntity<?> getOriginalUrl(@PathVariable String shortUrl) {
         Optional<UrlMapping> url = urlMappingService.getOriginalUrl(shortUrl);
 
         if (!url.isPresent()) {
@@ -39,13 +38,11 @@ public class UrlShortenerController {
     }
 
     /**
-     *
-     * @param urlMapping
+     * @param urlMapping :UrlMapping
      * @return Returns a shorted URL.
      */
     @PostMapping(path = "/shorten", consumes = "application/json", produces = "application/json")
-    @ResponseBody
-    public ResponseEntity shortenUrl(@RequestBody @NotNull UrlMapping urlMapping) {
+    public ResponseEntity<?> shortenUrl(@RequestBody @NotNull UrlMapping urlMapping) {
 
         Optional<UrlMapping> url = urlMappingService.generateShortenlUrl(urlMapping);
         if (url.isEmpty()) {
@@ -61,9 +58,9 @@ public class UrlShortenerController {
      * @param ex exception
      * @return Error message String
      */
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NoSuchElementException.class)
-//    public String trurn400(NoSuchElementException ex) {
-//        return ex.getMessage();
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String turn400(NoSuchElementException ex) {
+        return ex.getMessage();
+    }
 }
